d76a5aea06b789b4d117f7ff0b452ef033ac5f14
Wang Xuerui <idontknw.wang@gmail.com>
2015-09-06 10:06:22 +0800

    RIL: don't send the initial power off request for MTK
    Change-Id: I0fae0d5fb5b0fbb83e7cfe9205465af660a361a7

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -3277,7 +3277,7 @@ public final class RIL extends BaseCommands implements CommandsInterface {
                 if (RILJ_LOGD) unsljLogRet(response, ret);
 
                 // Initial conditions
-                setRadioPower(false, null);
+                // setRadioPower(false, null);
                 setCdmaSubscriptionSource(mCdmaSubscription, null);
                 setCellInfoListRate(Integer.MAX_VALUE, null);
                 notifyRegistrantsRilConnectionChanged(((int[])ret)[0]);
